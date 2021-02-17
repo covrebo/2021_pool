1b7b244072616a303674c7035752a631ec577882
Chris Ovrebo <chris.ovrebo@gmail.com>
2021-02-17 17:59:35 -0600

    Initial commit.

---
+++ app.py
@@ -0,0 +1,11 @@
+
+# TODO: check to be sure the data directory exists
+# TODO: print the header in the console
+# TODO: prompt the user for the race number
+# TODO: prompt the user for the results URL from ESPN
+# TODO: scrape ESPN results table and write results to CSV
+# TODO: import the picks for the week
+# TODO: import the weekly picks and convert to named tuple
+# TODO: import the standings and convert to named tuples
+# TODO: calculate the weekly points and new standings
+# TODO: write the new standing to csv
